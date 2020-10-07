34db9d8c82debc5e3e5d76cd37874b5302125ddf
BoredFish <49598984+BoredFishRE@users.noreply.github.com>
2020-10-07 12:18:38 -0400

    TheBotBot Upload

---
+++ commands/ping.js
@@ -0,0 +1,14 @@
+const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "ping",
+  description: "Ping tester",
+  execute(message, args, client) {
+    var ping = Math.round(client.ws.ping) + " ms";
+    let embed = new MessageEmbed()
+      .setColor("#42f2f5")
+      .addField("Pong!", ping, true)
+    message.channel.send(embed);
+  },
+};
---
+++ commands/userinfo.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "userinfo",
+  description: "Info about the user.",
+  execute(message, args, client) {
+    let embed = new MessageEmbed()
+      .setColor("#42f2f5")
+      .setThumbnail(message.author.displayAvatarURL())
+      .setTitle("Userinfo of " + message.author.username)
+      .setFooter(
+        "Requested by " + message.author.username,
+        message.author.displayAvatarURL()
+      )
+      .addField("User created at?", message.author.createdAt)
+      .addField("User Tag", message.author.tag)
+      .addField("User's Last message sent", message.author.lastMessage)
+      .addField("User ID", message.author.id)
+      .addField("User Bot?", message.author.bot)
+      .addField("User joined at?", message.guild.joinedAt);
+    message.channel.send(embed);
+  },
+};
